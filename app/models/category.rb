e7eb0ce15532da5a1bb974364d143099ac796523
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-22 12:54:44 -0700

    remove all reference to friendly_id gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
-  # extend FriendlyId
-  # friendly_id :name
+
   validates :name, presence: true, uniqueness: true
   has_many :items
   before_save :assign_slug
@@ -8,14 +7,5 @@ class Category < ApplicationRecord
   def assign_slug
     self.slug = self.name.parameterize if self.name
   end
-  # before_save :save_slug
-
-  # def save_slug
-  #   self.name.parameterize
-  # end
-
-  # def to_param
-  #   self.slug = self.name.parameterize
-  # end
 
 end
