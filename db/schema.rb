df9342e9cc1783e2ab88601ddc1c1a23c0caae51
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-21 18:08:08 -0700

    adds categories table, adds category reference and foreign key to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221225907) do
+ActiveRecord::Schema.define(version: 20170222010254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +26,9 @@ ActiveRecord::Schema.define(version: 20170221225907) do
     t.string   "image_tag"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
+  add_foreign_key "items", "categories"
 end
