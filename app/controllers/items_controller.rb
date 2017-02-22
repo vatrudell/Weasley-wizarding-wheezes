ea14809ec1200b52498b190aaedea28802e2a0af
Charlotte Moore <mooc123@gmail.com>
2017-02-22 11:07:13 -0700

    adds item show route, controller method, view, and test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    @cart = Cart.new(session[:cart])
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
   resources :carts, only: [:create]
   get '/cart', to: 'carts#show'
   root 'items#index'
