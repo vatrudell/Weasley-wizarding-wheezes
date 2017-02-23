7a2dc4ca85051a875b68596b2e22f276c386c964
Charlotte Moore <mooc123@gmail.com>
2017-02-22 20:38:34 -0700

    adds first and last name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, :password, :email, presence: true
+  validates :username, :first_name, :last_name, :password, :email, presence: true
   validates :username, :email, uniqueness: true
 end
