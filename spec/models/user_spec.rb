95530f87ceb8a0067f6aaffe25eb574c8f21a70c
CJ <mooc123@gmail.com>
2017-02-22 20:00:21 -0700

    Update user_spec.rb
    fixed indentation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
-     it { should have_secure_password }
+    it { should have_secure_password }
   end
 end
