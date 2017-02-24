7e92611445417777f727c2ae85e5bcef7650ca2c
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-24 15:18:56 -0800

    add test for cart checkout when cart has two items

--- spec/features/orders/authenticated_user_can_checkout_spec.rb
+++ spec/features/orders/authenticated_user_can_checkout_spec.rb
@@ -19,4 +19,25 @@ describe "when an authenticated user visits their cart" do
       expect(page).to have_content("Mischief is coming your way, you have successfully placed your order!")
     end
   end
+
+  it "they can checkout with two items" do
+  user = Fabricate(:user)
+  item = Fabricate(:item)
+  item2 = Fabricate(:item)
+  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+  visit item_path(item)
+  click_on "Add to Cart"
+  visit item_path(item2)
+  click_on "Add to Cart"
+  click_on "Cart"
+  click_on "Check Out"
+
+  expect(current_path).to eq(orders_path)
+  expect(page).to have_content("2")
+
+  within(".success") do
+    expect(page).to have_content("Mischief is coming your way, you have successfully placed your order!")
+  end
+end
 end
