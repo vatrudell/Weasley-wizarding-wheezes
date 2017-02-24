eb3fdde0b4ff5eecca42c691dcc5b19460eba2fa
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-24 08:42:28 -0800

    add shoulda-callback-matchers to gemfile, test callback in category model test

---
+++ spec/features/orders/authenticated_user_can_checkout_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+#
+# xdescribe "when an authenticated user visits their cart" do
+#   context "they can checkout and the order saves" do
+#     visit '/cart'
+#
+#     click_on "Check Out"
+#
+#     expect(current_path).to
+#   end
+# end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Category, type: :model do
-  context 'validations' do
+  context "validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  context "relationships" do
     it { should have_many(:items) }
+  end
+
+  context "callbacks" do
+    it { is_expected.to callback(:assign_slug).before(:save) }
   end
 end
