a56602976c1d96eca0fba90cf85b6e9b8c581f11
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-24 14:04:53 -0800

    starts cart checkout test

--- spec/fabricators/user_fabricator.rb
+++
@@ -1,5 +0,0 @@
-Fabricator(:user) do
-  username        "MyString"
-  email           "MyString"
-  password_digest "MyString"
-end
--- spec/features/orders/authenticated_user_can_checkout_spec.rb
+++ spec/features/orders/authenticated_user_can_checkout_spec.rb
@@ -1,11 +1,19 @@
-# require 'rails_helper'
-#
-# xdescribe "when an authenticated user visits their cart" do
-#   context "they can checkout and the order saves" do
-#     visit '/cart'
-#
-#     click_on "Check Out"
-#
-#     expect(current_path).to
-#   end
-# end
+require 'rails_helper'
+
+describe "when an authenticated user visits their cart" do
+  context "they can checkout and the order saves" do
+
+    user = Fabricate(:user)
+    item = Fabricate(:item)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    # allow_any_instance_of(ApplicationController).to receive(:set_cart) {}
+
+    visit items_path
+    click_on "Add to Cart"
+    click_on "Card"
+
+    click_on "Check Out"
+
+    expect(current_path).to
+  end
+end
