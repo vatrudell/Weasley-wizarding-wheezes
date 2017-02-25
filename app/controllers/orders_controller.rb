90b43f9b73f7f039c0617b7ee42bf7ceef61a692
Valerie Trudell <v.trudell@protonmail.com>
2017-02-24 18:31:12 -0700

    Pulls down master, pushing progress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,8 @@ class OrdersController < ApplicationController
   def index
     @orders = current_user.orders
   end
+
+  def show
+    @order = current_user.orders.find_by(params[:order_id])
+  end
 end
---
+++ spec/features/orders/user_can_view_a_single_order_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "when they visit the show page for an order" do
+  it "shows item title, price, quantity" do
+
+  end
+
+  it "" do 
+
+  end
+end
