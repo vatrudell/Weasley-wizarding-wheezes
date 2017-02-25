109a25db5811659f607891962cff74494d1f835c
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-24 18:16:54 -0700

    Add user gets 404 when visiting admin dashboard

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,6 @@ class Admin::BaseController < ApplicationController
   before_action :current_admin
 
   def current_admin
-    render file: 'public/404' unless current_admin?
+    render file: 'app/views/errors/not_found.html.erb' unless current_admin?
   end
 end
--- spec/features/users/admin_sees_dashboard_spec.rb
+++ spec/features/users/admin_sees_dashboard_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
-# As an Admin
-# When I visit "/admin/dashboard"
-# I will see a heading on the page that says "Admin Dashboard"
 # As a registered user
 # When I visit "/admin/dashboard"
 # I get a 404
@@ -19,13 +16,29 @@ describe "user visits admin dashboard" do
 
     expect(user.admin?).to eq(true)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    allow_any_instance_of(ApplicationController).to receive(:current_admin?).and_return(true)
 
     visit '/admin/dashboard'
-    expect(page).to have_content("Admin Dashboard")
+
+    within("h1") do
+      expect(page).to have_content("Admin Dashboard")
+    end
   end
 
   it "as a registered user" do
+    user = User.create(first_name: "Charlotte",
+                       last_name: "Moore",
+                       username: "Cj",
+                       email: "email@email.com",
+                       password: "password")
+
+    expect(user.admin?).to eq(false)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/admin/dashboard'
+
+    within("h1") do
+      expect(page).to have_content("Error: 404 page not found")
+    end
   end
 
   it "as an unregisterd user" do
