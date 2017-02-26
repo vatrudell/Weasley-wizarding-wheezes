46cc0c6ee4486b4b203133440a93b9a8faf6ae15
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-26 12:14:41 -0800

    adds category dropdown functionality, link working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
-
+  before_action :define_categories
   helper_method :current_user,
                 :current_admin?
 
@@ -20,4 +20,9 @@ class ApplicationController < ActionController::Base
   def admin_login
     redirect_to admin_dashboard_path if current_admin?
   end
+
+  def define_categories
+    @categories = Category.all
+  end
+
 end
