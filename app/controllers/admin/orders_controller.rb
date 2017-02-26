4cadf88dc37653e21440f1457ce456989d0b6d03
Charlotte Moore <mooc123@gmail.com>
2017-02-25 17:04:34 -0700

    update sets @orders and @status and renders the page again

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,32 +4,26 @@ class Admin::OrdersController < Admin::BaseController
     # user = User.find_by(username: params[:session][:username])
     # if user.current_admin
     # @orders = Order.all
-    session[:orders] = nil
-    @orders = get_orders
+    @orders = Order.all
     # byebug
       # byebug
     # else
     #   render:
     # end
     @status = Order.group(:status).count
+    byebug
   end
 
   def sort_orders
     # status = params[:order][:status]
-      session[:orders] = Order.get_by_status(params[:order][:status])
+      @orders = Order.get_by_status(params[:order][:status])
+      @status =Order.all.group(:status).count
       # byebug
-      redirect_to admin_dashboard_path
+      render 'admin/orders/index'
     # else
       # session[:orders] = nil
     # end
   end
 
-  def get_orders
-    if session[:orders] == nil || session[:orders] == []
-      Order.all
-    else
-      # byebug
-      session[:orders]
-    end
-  end
+
 end
