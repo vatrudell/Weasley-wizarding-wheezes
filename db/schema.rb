6d31a9bc953c6c95666eee80b094628757a97391
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-26 14:47:28 -0800

    add 12factor gem for production and paperclip likely successfully implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224183225) do
+ActiveRecord::Schema.define(version: 20170226223536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,14 @@ ActiveRecord::Schema.define(version: 20170224183225) do
     t.string   "description"
     t.float    "price"
     t.string   "image_tag"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "category_id"
-    t.integer  "item_status", default: 0
+    t.integer  "item_status",        default: 0
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
