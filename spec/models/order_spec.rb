b70be1e80d350811b9947a4e05778ca952d4c036
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-26 11:32:25 -0700

    Add test for submit-order method to order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe Order, type: :model do
   context "process" do
     it { should define_enum_for(:status)}
   end
+
+  it ".submit_order creates order & order-items from cart" do
+    user = Fabricate(:user)
+    item1 = Fabricate(:item)
+    item2 = Fabricate(:item)
+    cart = Cart.new({})
+    cart.add_item(item1.id)
+    cart.add_item(item1.id)
+    cart.add_item(item2.id)
+
+    order = user.orders.create!(total_price: cart.total)
+    order.submit
+    
+    expect(order.persisted?).to eq(true)
+    expect(order.order_items.count).to eq(2)
+    expect(OrderItem.find_by(item: item1).quantity).to eq(2)
+    expect(OrderItem.find_by(item: item2).quantity).to eq(1)
+  end
 end
