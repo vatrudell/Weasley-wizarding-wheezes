1603582abd514358dd435a7418381aaceab0a7b0
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-27 12:56:21 -0700

    add passing tests for admin item#new and item#edit

--- spec/features/admin/items/admin_can_edit_item_spec.rb
+++ spec/features/admin/items/admin_can_edit_item_spec.rb
@@ -1,33 +1,25 @@
 require 'rails_helper'
 
-xdescribe "when admin visits the items page" do
-  it "admin can edit an item" do
+describe "when admin visits the items page and selects edit" do
+  it "admin can edit an item title, description" do
+    item = Fabricate(:item)
     adminda = Fabricate(:user)
     adminda.admin!
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(adminda)
-    visit edit_item_path
 
-    expect(page).to have_content("Create A New Item")
+    visit admin_items_path
+    click_on "Edit"
 
-    category = Category.create(name: "Postions")
-    item_one = Item.new(title: "Love Posions",
-                    description: "For all your foolish needs",
-                    price: 12.78,
-                    image_tage: "http://harrypotter.com/love_postion")  #you're going to want to change this
-    item_two = Item.new(title: "Hair Growing Posions",
-                    description: "Grow your hair, grow your teachers hair, get it here",
-                    price: 120.78,
-                    image_tage: "http://harrypotter.com/hair_growing_postion")  #you're going to want to change this
-    fill_in "item[title]", with: item_two.title
-    fill_in "item[description]", with: item_two.description
-    fill_in "item[price]", with: item_two.price
-    fill_in "item[image_tage]", with: item_two.image_tag
-    fill_in "item[title]", with: category.id
+    expect(page).to have_content("Edit #{item.title}")
 
-    click "Submit"
+    fill_in "item[title]", with: "Test item"
+    fill_in "item[description]", with: "Test description"
+    fill_in "item[price]", with: "40"
 
-    expect(current_page).to be(items_path)
-    expect(page).to have_content(item_two.title)
-    expect(page).to_not have_content(item_one.title)
+    click_on "Submit"
+
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("Test item")
+    expect(page).to_not have_content(item.title)
   end
 end
