b1a666382d1da16ba7c68130bc4a04641c0c6f60
Charlotte Moore <mooc123@gmail.com>
2017-02-26 21:24:25 -0700

    adds sad path test for admin items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,15 @@
 class Admin::ItemsController < Admin::BaseController
+  include ActionView::Helpers::TextHelper
 
   def index
     @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
   end
 
 end
