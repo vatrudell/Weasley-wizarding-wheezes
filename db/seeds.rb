1e8bd7f3d170fb6a83fb06ce6b802624dd141104
Charlotte Moore <mooc123@gmail.com>
2017-02-28 14:16:33 -0700

    rearranges seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,9 @@ def import_product_csv
   puts "Products have loaded!"
 end
 
-Order.delete_all
-User.delete_all
-Item.delete_all
-OrderItem.delete_all
-Category.delete_all
-import_product_csv
+
+def create_users
+
 
 user = User.create!(username: "user",
                     email: "email@email.com",
@@ -39,26 +36,35 @@ admin = User.create!(username: "admin",
                      role: 1)
 
 puts "Admin Created!"
+end
 
-item1 = Item.find(1)
-item2 = Item.find(2)
-item3 = Item.find(3)
-item4 = Item.find(4)
-item5 = Item.find(5)
+def create_orders
 
-order1 = user.orders.create!(total_price: (item1.price + item2.price + item3.price).round(2))
-order1.order_items.create!(item: item1, quantity: 1, price: item1.price)
-order1.order_items.create!(item: item2, quantity: 1, price: item2.price)
-order1.order_items.create!(item: item3, quantity: 1, price: item3.price)
+order1 = User.find_by(username: "user").orders.create!(total_price: (Item.find(1).price + Item.find(2).price + Item.find(3).price).round(2))
+order1.order_items.create!(item: Item.find(1), quantity: 1, price: Item.find(1).price)
+order1.order_items.create!(item: Item.find(2), quantity: 1, price: Item.find(2).price)
+order1.order_items.create!(item: Item.find(3), quantity: 1, price: Item.find(3).price)
 puts "Order 1 added!"
 
-order2 = user.orders.create!(total_price: (item3.price + item4.price + item5.price).round(2))
-order2.order_items.create!(item: item3, quantity: 1, price: item3.price)
-order2.order_items.create!(item: item4, quantity: 1, price: item4.price)
-order2.order_items.create!(item: item5, quantity: 1, price: item5.price)
+order2 = User.find_by(username: "user").orders.create!(total_price: (Item.find(3).price + Item.find(4).price + Item.find(5).price).round(2))
+order2.order_items.create!(item: Item.find(3), quantity: 1, price: Item.find(3).price)
+order2.order_items.create!(item: Item.find(4), quantity: 1, price: Item.find(4).price)
+order2.order_items.create!(item: Item.find(5), quantity: 1, price: Item.find(5).price)
 puts "Order 2 added!"
 
-order3 = user.orders.create!(total_price: (item3.price * 2 + item5.price).round(2), status: "canceled")
-order3.order_items.create!(item: item3, quantity: 2, price: item3.price)
-order3.order_items.create!(item: item5, quantity: 1, price: item5.price)
+order3 = User.find_by(username: "user").orders.create!(total_price: (Item.find(3).price * 2 + Item.find(5).price).round(2), status: "canceled")
+order3.order_items.create!(item: Item.find(3), quantity: 2, price: Item.find(3).price)
+order3.order_items.create!(item: Item.find(5), quantity: 1, price: Item.find(5).price)
 puts "Order 3 added!"
+
+end
+
+OrderItem.delete_all
+Item.delete_all
+Order.delete_all
+User.delete_all
+Category.delete_all
+
+import_product_csv
+create_users
+create_orders
